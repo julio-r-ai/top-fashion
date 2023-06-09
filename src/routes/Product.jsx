4e9f99b1c8bc5c40ad92196ec624ad2c8b673a5c
Julio Cesar <julioreis044@gmail.com>
2023-06-09 01:17:54 -0300

    Desenvolvendo pagina produto

--- src/routes/Product.jsx
+++ src/routes/Product.jsx
@@ -8,6 +8,31 @@ const Product = () => {
         <motion.div animate={{  y: 30 }} transition={{ duration: 0.8 }}>
             <h1>Produtos</h1>
         </motion.div>
+
+        <motion.div className="register">
+           <h2>Cadastrar novo produto</h2>
+           <div>
+                <label htmlFor="">Nome</label>
+                <input type="text"></input>
+           </div>
+           <div>
+                <label htmlFor="">Preco</label>
+                <input type="text" />
+           </div>
+           <div>
+                <label htmlFor="">Adicione uma imagem</label>
+                <input type="file" />
+           </div>
+           <div>
+                <button>Cadastrar</button>
+           </div>
+        </motion.div>
+
+        <motion.div className="search">
+            <label htmlFor="">Fazer busca</label>
+            <input type="text"></input>
+            <button>Buscar</button>
+        </motion.div>
            
        </div>
    )
