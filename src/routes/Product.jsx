ad607ecfcb01612024dfe5cb4994bde0a32d6b2f
Julio Cesar <julioreis044@gmail.com>
2023-06-16 23:17:46 -0300

    Finalizando formulário de cadastro

--- src/routes/Product.jsx
+++ src/routes/Product.jsx
@@ -10,14 +10,39 @@ const Product = () => {
         </motion.div>
 
         <motion.div className="register">
+           <div className="registerInterno">
            <h2>Cadastrar novo produto</h2>
            <div>
-                <label htmlFor="">Nome</label>
-                <input type="text"></input>
+                <label htmlFor="">Descrição</label>
+                <input type="text" name="description"></input>
            </div>
            <div>
-                <label htmlFor="">Preco</label>
-                <input type="text" />
+                <label htmlFor="">Preço</label>
+                <input type="text" name="price"/>
+           </div>
+           <div>
+                <label htmlFor="">Cor</label>
+               <select name="departamento">
+                    <option selected disabled>Selecione</option>
+                    <option value="red">Vermelho</option>
+                    <option value="blue">Azul</option>
+                    <option value="pink">Rosa</option>
+                    <option value="yelow">Amarelho</option>
+                    <option value="White">Branco</option>
+                    <option value="Black">Preto</option>
+               </select>
+           </div>
+           <div>
+                <label htmlFor="">Tamanho</label>
+               <select name="departamento">
+                    <option selected disabled>Selecione</option>
+                    <option value="6">P</option>
+                    <option value="5">PP</option>
+                    <option value="4">M</option>
+                    <option value="3">G</option>
+                    <option value="2">GG</option>
+                    <option value="1">XGG</option>
+               </select>
            </div>
            <div>
                 <label htmlFor="">Adicione uma imagem</label>
@@ -25,6 +50,7 @@ const Product = () => {
            </div>
            <div>
                 <button>Cadastrar</button>
+           </div>
            </div>
         </motion.div>
 
